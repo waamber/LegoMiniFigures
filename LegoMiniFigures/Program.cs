35c5084951f717265b68cdbe117456864fa73e5e
waamber <amberastuart@aol.com>
2018-01-21 14:15:10 -0600

    setup

---
+++ LegoMiniFigures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoMiniFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+   
+}
